28e527e18c48138765b659df0b5cb07c5e9b7fa3
Ahmad Fajar Prasetiyo <me@ahmadfajar.com>
2023-11-09 13:49:41 +0700

    Stage 2/7: Grader

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,17 +1,35 @@
 package bullscows;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        final String secretCode = "9305";
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        String answer = scanner.nextLine();
+        int bulls = 0;
+        int cows = 0;
 
-        System.out.println("The secret code is prepared: ****.");
-        System.out.println();
-        System.out.println("Turn 1. Answer:");
-        System.out.println("1234");
-        System.out.println("Grade: None.");
-        System.out.println();
-        System.out.println("Turn 2. Answer:");
-        System.out.println("9876");
-        System.out.println("Grade: 4 bulls.");
-        System.out.println("Congrats! The secret code is 9876.");
+        for(int i = 0; i < secretCode.length(); i++) {
+            int indexAnswer = answer.indexOf(secretCode.charAt(i));
+            if(i == indexAnswer) {
+                bulls++;
+            } else if(indexAnswer != -1) {
+                cows++;
+            }
+        }
+
+        if(bulls > 0 && cows > 0) {
+            System.out.printf("Grade: %d bull(s) and %d cow(s).", bulls, cows);
+        } else if(bulls > 0) {
+            System.out.printf("Grade: %d bull(s).", bulls);
+        } else if(cows > 0) {
+            System.out.printf("Grade: %d cow(s).", cows);
+        } else {
+            System.out.print("Grade: None.");
+        }
+
+        System.out.printf(" The secret code is %s.\n", secretCode);
     }
 }
